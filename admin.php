84448298f87b88e181264f60e76f63734095e02c
ChrisLinn <nanyishanshaoxiu@gmail.com>
2013-08-08 07:23:08 +0800

    a teacher-friendly admin.php 1

--- admin.php
+++ admin.php
@@ -1,1 +1,120 @@
-﻿<!DOCTYPE html><html dir="ltr" lang="zh-CN"><!--<![endif]--><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta charset="UTF-8"><meta name="viewport" content="width=device-width"><title>中国石油大学(华东)电气创新实验室预约系统</title><link rel="stylesheet" type="text/css" media="all" href="./BenOrSeattle_files/style.css"><link rel="stylesheet" type="text/css" media="all" href="./BenOrSeattle_files/admin-bar.min.css"><link href="./index_files/favicon.ico?v=20130411" type="image/x-icon" rel="shortcut icon"><!--[if lt IE 9]><script src="http://eeilupc.duapp.com/wp-content/themes/twentyeleven/js/html5.js" type="text/javascript"></script><![endif]--><meta name="generator" content="WordPress 3.3.1"><script type="text/javascript" src="./BenOrSeattle_files/shCore.js"></script><link type="text/css" rel="stylesheet" href="./BenOrSeattle_files/shCoreDefault.css">	<style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}.STYLE1 {color: #666666}</style></head><body class="home blog single-author two-column right-sidebar"><div id="page" class="hfeed">	<header id="branding" role="banner">	  <hgroup class="entry-title">	    <p class="entry-title STYLE1">EEIL in UPC Booking Sys </p>	  </hgroup>															<img src="./BenOrSeattle_files/trolley.jpg" width="1000" height="288" alt="">	  </a>																	<!-- #access -->	</header>	<!-- #branding -->	<div id="main" style="text-align:center; vertical-align:middle; line-height:24px">		<div id="primary">							<article id="post-0" class="post no-results not-found">					<header class="entry-header">											</header><!-- .entry-header -->					<div class="entry-content" >						<!--------------------------------------------------内容是在这添加么-------------------------------------------------------------------------------------------------------------------------------->Welcome~ <?php echo $_COOKIE["id"]; ?>.<br /><?phpinclude 'header.php';if(!isset($_COOKIE["id"])) Header("Location:index.php");$userid =$_COOKIE["id"];if(con2db()){$exec = "SELECT priority FROM user WHERE Id='$userid'";$pri =mysql_result(mysql_query($exec),0);if($pri!=5) Header("Location:index.php");}?>欢迎管理员～</br></br><h4>修改用户身份</h4><form action="admin2.php" method="post">TA的学号：<input style="width:8%" type="text" name="Oid" /><select name="Opri"><option value="1">普通用户</option><option value="2">创新实验室成员</option><option value="5">管理员</option></select><input value="选择" type="submit" /></form><br/><br/><form action="answer.php" method="get"><h4>要查询的日期:</h4>格式如：“20120128”<br/><input style="width:8%" type="text" name="date" /><input value="选择" type="submit" /></form><br/>你的身份是：<?php$exec = "SELECT status FROM user WHERE Id='$userid'";echo $status =mysql_result(mysql_query($exec),0);?><br/><a href="/reborn.php">修改个人信息</a>  <a href="/logout.php">注销</a>					</div><!-- .entry-content -->				</article><!-- #post-0 -->					</div><!-- #primary -->		<!-- #secondary .widget-area -->	</div><!-- #main -->	<footer id="colophon" role="contentinfo">							</footer><!-- #colophon --></div><!-- #page --><script type="text/javascript">SyntaxHighlighter.all();</script><div id="leading" >				<div id="wpadminbar" class="nojq nojs" role="navigation">		<a class="screen-reader-shortcut" href="http://localhost/wordpress/#wp-toolbar" tabindex="1">跳至工具栏</a>		<div class="quicklinks" id="wp-toolbar" role="navigation" aria-label="顶部导航工具栏。" tabindex="0">		<ul id="wp-admin-bar-top-secondary" class="ab-top-secondary ab-top-menu">		<li id="wp-admin-bar-my-account" class="menupop with-avatar"><a class="ab-item" aria-haspopup="true"  title="我的帐户">您好，<?php echo $_COOKIE["id"]; ?><img alt="" src="./BenOrSeattle_files/noavatar_small.gif" class="avatar avatar-16 photo" height="16" width="16"></a><div class="ab-sub-wrapper"><ul id="wp-admin-bar-user-actions" class="ab-submenu">		<li id="wp-admin-bar-user-info"><a class="ab-item" tabindex="-1" ><img alt="" src="./BenOrSeattle_files/noavatar_middle.gif" class="avatar avatar-64 photo" height="64" width="64"><span class="display-name"><?php showmyname() ?>></span></a>		</li>		<li id="wp-admin-bar-edit-profile"><a ><?php showmysta() ?></a>		</li>		<li id="wp-admin-bar-edit-profile"><a class="ab-item" href="/mybooking.php">我的预约</a>		</li>		<li id="wp-admin-bar-edit-profile"><a class="ab-item" href="/reborn.php">修改个人信息</a>		</li>		<li id="wp-admin-bar-logout"><a class="ab-item" href="/logout.php">注销</a>		</li></ul></div>		</li></ul>			</div>		</div>		</div></body></html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
+<!-- saved from url=(0048)http://auto.upc.edu.cn/dgdzkc/yuyue/student.html -->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><HTML 
+xmlns="http://www.w3.org/1999/xhtml"><HEAD>
+<META content="text/html; charset=gb2312" http-equiv="Content-Type">
+<META name="description" content="student"><TITLE>�繤����ѧʵ��ԤԼϵͳ</TITLE><LINK rel="stylesheet" 
+type="text/css" href="admin_files/style.css"><LINK rel="stylesheet" type="text/css" 
+href="admin_files/main.css">
+<SCRIPT type="text/javascript" src="admin_files/function.js"></SCRIPT>
+<?php include 'header.php'; ?>
+<META name="GENERATOR" content="MSHTML 9.00.8112.16496"></HEAD>
+<BODY>
+<DIV id="wraper">
+<DIV id="main_top" onselectstart="return false">
+<DIV id="top_logo"></DIV>
+<DIV id="top_bar"></DIV><IMG hideFocus="true" id="nextNote" alt="��һ��" align="absmiddle" 
+src="admin_files/next.jpg"></DIV>
+<DIV id="main_left"><!--�������ݿ�ʼ-->
+<TABLE onselectstart="return false" class="table_left" cellSpacing="0" 
+cellPadding="0">
+  <TBODY>
+  <TR class="titleTR">
+    <TD height="30" background="admin_files/top_left.gif" width="10"></TD>
+    <TD background="admin_files/top_bg.gif">
+      <DIV class="table_title">�û���Ϣ</DIV>
+      <DIV class="slide"></DIV></TD>
+    <TD background="admin_files/top_right.gif" width="10"></TD></TR>
+  <TR>
+    <TD background="admin_files/leftbg.gif" width="10"></TD>
+    <TD bgColor="#ffffff" vAlign="top">
+      <UL class="list">
+        <LI id="usertype"><B>�� �ݣ�</B><?php showmysta()?></LI>
+        <LI id="realname"><B>�� ����</B><?php showmyname()?></LI>
+        <LI id="username"><B>ѧ �ţ�</B><?php echo $_COOKIE["id"]; ?></LI>
+        <LI><B><a  href="/reborn.php">�޸ĸ�����Ϣ</a>		</B>&nbsp;|&nbsp;<B><a  href="/logout.php">ע��</a>		</B></LI></UL></TD>
+    <TD background="admin_files/rightbg.gif" width="10"></TD></TR>
+  <TR>
+    <TD height="20" background="admin_files/bottom_left.gif" 
+    width="10"></TD>
+    <TD background="admin_files/bottom_bg.gif">&nbsp;</TD>
+    <TD background="admin_files/bottom_right.gif" width="10"></TD></TR>
+  </TBODY></TABLE>
+<TABLE onselectstart="return false" class="table_left" cellSpacing="0" 
+cellPadding="0">
+  <TBODY>
+  <TR class="titleTR">
+    <TD height="30" background="admin_files/top_left.gif" width="10"></TD>
+    <TD background="admin_files/top_bg.gif">
+      <DIV class="table_title">ʵ�����</DIV>
+      <DIV class="slide"></DIV></TD>
+    <TD background="admin_files/top_right.gif" width="10"></TD></TR>
+  <TR>
+    <TD background="admin_files/leftbg.gif" width="10"></TD>
+    <TD bgColor="#ffffff" vAlign="top">
+      <UL style="height: 90px;" class="list">
+        <LI><IMG align="absmiddle" src="admin_files/list.gif">&nbsp;<B><A 
+        href="http://auto.upc.edu.cn/dgdzkc/yuyue/st_explist.html" target="main_Frame">��Ա����</A></B></LI>
+        <LI><IMG align="absmiddle" src="admin_files/list.gif">&nbsp;<B><A 
+        href="http://auto.upc.edu.cn/dgdzkc/yuyue/st_expmanage.html" target="main_Frame">ԤԼ�鿴</A></B></LI>
+        <LI><IMG align="absmiddle" src="admin_files/list.gif">&nbsp;<B><A 
+        href="http://auto.upc.edu.cn/dgdzkc/yuyue/st_expfinish.html" target="main_Frame">ԤԼ����</A></B></LI>
+        <LI><IMG align="absmiddle" src="admin_files/list.gif">&nbsp;<B><A 
+        href="http://auto.upc.edu.cn/dgdzkc/yuyue/st_expscore.html" target="main_Frame">ԤԼȴ����</A></B></LI></UL></TD>
+    <TD background="admin_files/rightbg.gif" width="10"></TD></TR>
+  <TR>
+    <TD height="20" background="admin_files/bottom_left.gif" 
+    width="10"></TD>
+    <TD background="admin_files/bottom_bg.gif">&nbsp;</TD>
+    <TD background="admin_files/bottom_right.gif" width="10"></TD></TR>
+  </TBODY></TABLE>
+<TABLE onselectstart="return false" class="table_left" cellSpacing="0" 
+cellPadding="0">
+  <TBODY>
+  <TR class="titleTR">
+    <TD height="30" background="admin_files/top_left.gif" width="10"></TD>
+    <TD background="admin_files/top_bg.gif">
+      <DIV class="table_title">��Ϣ����</DIV>
+      <DIV class="slide"></DIV></TD>
+    <TD background="admin_files/top_right.gif" width="10"></TD></TR>
+  <TR>
+    <TD background="admin_files/leftbg.gif" width="10"></TD>
+    <TD bgColor="#ffffff" vAlign="top">
+      <UL style="height: 25px;" class="list">
+        <LI><IMG align="absmiddle" src="admin_files/list.gif">&nbsp;<B><A 
+        href="admin_files/st_note.htm" 
+        target="main_Frame">��վ�����б�</A></B></LI></UL></TD>
+    <TD background="admin_files/rightbg.gif" width="10"></TD></TR>
+  <TR>
+    <TD height="20" background="admin_files/bottom_left.gif" 
+    width="10"></TD>
+    <TD background="admin_files/bottom_bg.gif">&nbsp;</TD>
+    <TD background="admin_files/bottom_right.gif" width="10"></TD></TR>
+  </TBODY></TABLE><!--�������ݽ���--></DIV>
+<DIV id="main_right"><B 
+style="height: 0px; overflow: hidden; float: right;"></B>
+<DIV id="right_1"><IMG id="middle_img" src="admin_files/middle.gif"></DIV>
+<DIV id="right_2"><IFRAME id="main_Frame" src="admin_files/st_note.htm" 
+frameBorder="0" name="main_Frame" scrolling="yes"></IFRAME></DIV></DIV>
+<DIV id="noteview">
+<TABLE border="0" cellSpacing="0" cellPadding="0" width="100%">
+  <TBODY>
+  <TR>
+    <TD height="30" background="admin_files/top_left.gif" width="10"></TD>
+    <TD background="admin_files/top_bg.gif">
+      <DIV class="table_title"></DIV>
+      <DIV class="closediv"></DIV></TD>
+    <TD background="admin_files/top_right.gif" width="10"></TD></TR>
+  <TR>
+    <TD background="admin_files/leftbg.gif" width="10"></TD>
+    <TD bgColor="#ffffff" vAlign="top">
+      <DIV id="notecontent"></DIV></TD>
+    <TD background="admin_files/rightbg.gif" width="10"></TD></TR>
+  <TR>
+    <TD height="20" background="admin_files/bottom_left.gif" 
+    width="10"></TD>
+    <TD background="admin_files/bottom_bg.gif">&nbsp;</TD>
+    <TD background="admin_files/bottom_right.gif" width="10"></TD></TR>
+  </TBODY></TABLE></DIV></DIV>
+<DIV id="main_animate"></DIV>
+<DIV id="toTop"><IMG src="admin_files/toTop.gif"></DIV></BODY></HTML>
